2eee8e02d2748dc6c9ab87b2f4537c4ed6be27e1
Gökhan Öztürkoğlu <gokhan@snapbytes.com>
2020-05-13 03:13:43 +0300

    Exception handler classes added

--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,9 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import ProjectItem from "./Project/ProjecItem";
+import CreateProjectButton from "./Project/CreateProjectButton";
 
-export default class Dashboard extends Component {
-    render() {
-        return (
-          <h1>Welcome to the Dashboard</h1>
-        )
-    }
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className="projects">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h1 className="display-4 text-center">Projects</h1>
+              <br />
+              <a >
+                <CreateProjectButton/>
+              </a>
+              <br />
+              <hr />
+              <div>
+                <ProjectItem/>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
+
+export default Dashboard;
---
+++ ppmtool-react-client/src/components/Project/AddProject.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
+
+class AddProject extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Add Project Form</h1>
+            </div>
+        )
+    }
+}
+
+export default AddProject;
